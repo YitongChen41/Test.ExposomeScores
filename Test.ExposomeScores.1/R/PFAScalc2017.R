407d2844dd6d953d7f93fc4ccf4faab9fc4e3650
YitongChen41 <yitong.chen@mountsinai.org>
2025-08-12 14:41:20 -0400

    Add files via upload

--- Test.ExposomeScores.1/R/PFAScalc2017.R
+++ Test.ExposomeScores.1/R/PFAScalc2017.R
@@ -2,24 +2,24 @@ PFAScalc2017<-function(data,me.PFOSA.AcOH,PFUA,PFDeA,PFHxS,PFNA,PFOA,PFOS){
   if (!requireNamespace("mirt", quietly = TRUE)) {
     stop("Package 'mirt' is required but not installed.")
   }
-  
-  data("WtdQuantile", package = "Test.ExposomeScores", envir = environment())
-  data("mod.mirt.sum", package = "Test.ExposomeScores", envir = environment())
-  data("mod.mirt.isomer", package = "Test.ExposomeScores", envir = environment())
-  
+
+  data("WtdQuantile", package = "Test.ExposomeScores.1", envir = environment())
+  data("mod.mirt.sum", package = "Test.ExposomeScores.1", envir = environment())
+  data("mod.mirt.isomer", package = "Test.ExposomeScores.1", envir = environment())
+
   data.cont<-data[,c(me.PFOSA.AcOH,PFUA,PFDeA,PFHxS,PFNA,PFOA,PFOS)]
   nm.7pfas<-c("me.PFOSA.AcOH","PFUA","PFDeA","PFHxS","PFNA","PFOA","PFOS")
   colnames(data.cont)<-nm.7pfas
-  
+
   data.cut<-data.cont
-  
+
   for (pfas.i in nm.7pfas){
     data.cut[,pfas.i]=as.numeric(cut(data.cont[,pfas.i],WtdQuantile[[pfas.i]],right=FALSE,labels=c(1:(length(WtdQuantile[[pfas.i]])-1))))
   }
-  
-  
+
+
   fs.output<-mirt::fscores(mod.mirt.sum,response.pattern=data.cut)
   return(fs.output)
-  
-  
+
+
 }
